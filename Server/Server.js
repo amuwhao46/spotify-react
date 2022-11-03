727d5cf1b2294a7b016200b2fcfeae5647ca5c06
Oke Amuwha <amuwhao46@gmail.com>
2022-11-03 15:54:47 -0400

    set up env file

--- Server/Server.js
+++ Server/Server.js
@@ -1,3 +1,4 @@
+require('dotenv') .config()
 const express = require('express');
 const SpotifyWebApi = require('spotify-web-api-node');
 const cors = require("cors");
@@ -7,13 +8,15 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json())
 
+
+
 app.post('/refresh', (req, res) => {
 
     const refreshToken = req.body.refreshToken
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000',
-        clientId: 'unknown',
-        clientSecret: 'unknown',
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
         refreshToken,
     })
 
@@ -25,20 +28,18 @@ app.post('/refresh', (req, res) => {
                 expiresIn: data.body.expires_in,
             })
         })
-        .catch(err => {
-            console.log(err)
+        .catch(() => {
+            //console.log(err)
             res.sendStatus(400)
         }); 
 })
 
-
-
 app.post('/login', (req, res) => {
     const code = req.body.code;
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000',
-        clientId: 'unknown',
-        clientSecret: 'unknown',
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
     })
 
     spotifyApi.authorizationCodeGrant(code)
@@ -49,8 +50,8 @@ app.post('/login', (req, res) => {
             expiresIn: data.body.expires_in,
         })
     })
-    .catch(err => {
-        console.log(err)
+    .catch(() => {
+        //console.log(err)
         res.sendStatus(400)
     })
 })
